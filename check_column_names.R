a6cb64231af68d64682ff32f01b5620638a4b67f
ThiagoBrommonschenkel <Thiago.brommonschenkel@gmail.com>
2024-03-13 17:03:59 -0300

    Update check_column_names.R

--- check_column_names.R
+++ check_column_names.R
@@ -1,15 +1,15 @@
 check_column_names <- function(df1, df2) {
   # Checks if the column names are the same (ignoring case)
-  iguais <- all(tolower(colnames(df1)) == tolower(colnames(df2)))
+  equals <- all(tolower(colnames(df1)) == tolower(colnames(df2)))
   
   # If column names are not the same, identify differences
   if (!iguais) {
     # Get the column names of each dataframe
-    nomes_df1 <- tolower(colnames(df1))
-    nomes_df2 <- tolower(colnames(df2))
+    names_df1 <- tolower(colnames(df1))
+    names_df2 <- tolower(colnames(df2))
     
     # Check which column names are different
-    diferentes <- which(nomes_df1 != nomes_df2)
+    differents <- which(nomes_df1 != nomes_df2)
     
     # Show the names of different columns
     cat("The following column names are different:\n")
